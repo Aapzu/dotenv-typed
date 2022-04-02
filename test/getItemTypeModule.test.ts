c3b0ae986ba2df9c54ffa8bd8a4743451adb0803
Aapeli Haanpuu <aapeli@swappie.com>
2022-04-02 23:18:50 +0300

    Update getItemTypeModule.test.ts

--- test/getItemTypeModule.test.ts
+++ test/getItemTypeModule.test.ts
@@ -1,6 +1,4 @@
 import { getItemTypeModule } from '../src/getItemTypeModule'
-
-import arrayModule from '../src/typeModules/array'
 import booleanModule from '../src/typeModules/boolean'
 import enumModule from '../src/typeModules/enum'
 import numberModule from '../src/typeModules/number'
@@ -8,15 +6,21 @@ import stringModule from '../src/typeModules/string'
 
 describe('getItemTypeModule', () => {
   it.each`
-    itemType                    | expectedModule                | name
-    ${{ type: Boolean }}        | ${booleanModule}              | ${'booleanModule'}
-    ${{ type: Array(Boolean) }} | ${arrayModule(booleanModule)} | ${'booleanArrayModule'}
-    ${{ type: ['foo', 'bar'] }} | ${enumModule}                 | ${'enumModule'}
-    ${{ type: Number }}         | ${numberModule}               | ${'numberModule'}
-    ${{ type: Array(Number) }}  | ${arrayModule(numberModule)}  | ${'numberArrayModule'}
-    ${{ type: String }}         | ${stringModule}               | ${'stringModule'}
-    ${{ type: Array(String) }}  | ${arrayModule(stringModule)}  | ${'stringArrayModule'}
+    itemType                    | expectedModule   | name
+    ${{ type: Boolean }}        | ${booleanModule} | ${'booleanModule'}
+    ${{ type: ['foo', 'bar'] }} | ${enumModule}    | ${'enumModule'}
+    ${{ type: Number }}         | ${numberModule}  | ${'numberModule'}
+    ${{ type: String }}         | ${stringModule}  | ${'stringModule'}
   `('recognizes $name', ({ itemType, expectedModule }) => {
     expect(getItemTypeModule(itemType)).toBe(expectedModule)
+  })
+
+  it.each`
+    itemType                    | expectedModuleType | name
+    ${{ type: Array(Boolean) }} | ${'boolean array'} | ${'booleanArrayModule'}
+    ${{ type: Array(Number) }}  | ${'number array'}  | ${'numberArrayModule'}
+    ${{ type: Array(String) }}  | ${'string array'}  | ${'stringArrayModule'}
+  `('recognizes $name', ({ itemType, expectedModuleType }) => {
+    expect(getItemTypeModule(itemType).typeName).toBe(expectedModuleType)
   })
 })
